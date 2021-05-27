f228a77e43dfb1ac953851fbcccfeb21df056426
Poonam Raghuvanshi <raghuvanshipoonam96@gmail.com>
2021-05-28 00:04:31 +0530

    Card Css Added

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
-import ExpenseItem from './Components/ExpenseItem';
+import Expense from './Components/Expense';
 
 function App() {
-  const expense = [
+  const expenses = [
     {
       id: "1",
       date: new Date(),
-      title: "Car Insurance",
+      title: "Tv Showroom",
       amount: "294.67"
     }, {
       id: "2",
       date: new Date(2021, 2, 5),
-      title: "Car Insurance",
+      title: "Electricity Bill",
       amount: "104.37"
     }, {
       id: "3",
@@ -22,15 +22,12 @@ function App() {
     }, {
       id: "4",
       date: new Date(2021, 1, 15),
-      title: "Car Insurance",
+      title: "Bike Service",
       amount: "694.67"
     }]
   return (
     <div>
-      <ExpenseItem date={expense[0].date} title={expense[0].title} amount={expense[0].amount}></ExpenseItem>
-      <ExpenseItem date={expense[1].date} title={expense[1].title} amount={expense[1].amount}></ExpenseItem>
-      <ExpenseItem date={expense[2].date} title={expense[2].title} amount={expense[2].amount}></ExpenseItem>
-      <ExpenseItem date={expense[3].date} title={expense[3].title} amount={expense[3].amount}></ExpenseItem>
+     <Expense items={expenses}/>
     </div>
   );
 }
